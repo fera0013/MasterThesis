f1f49faf89db418f7329b6c98d26534b3ce24fb8
fera0013 <ralphfehrer@web.de>
2013-03-03 11:31:10 +0100

    Adaptions and modifications for publishing

--- OutlierDetectionMatlabWorkspace/RollerBearingOutlierDetectionSample.m
+++ OutlierDetectionMatlabWorkspace/RollerBearingOutlierDetectionSample.m
@@ -1,33 +1,17 @@
 %% Support Vector Data Description (SVDD) of Roller Bearing time series
-% Example of an outlier detection problem with roller bearing data
-% Involves the following steps:
-%  - Preprocessing (1)
-%    + Segmentation of roller bearing time series data 
-%    + Further segmentation of each segment into frames
-%  - Feature Extraction (1)
-%    + Kurtosis of each segment
-%    + Mel Frequency Cepstrum of each frame
-%    + Higuchi Fractal Dimensions of each frame
-%  - Semisupervised Classification
-%    + Support Vector Data Description (2)
-%  - Error evaluation
-%    + false negative and false positive rate
-%    + Roc Curve
+% Detection of Outliers in Rolling Element Bearing Datasets using Support
+% Vector Data Description
 %% SVDD sample
 % Creates a banana-shaped one-class dataset and calculates three different
-% SVDD mappings with Radial Basis Functions with different Sigma values
-
+% SVDD mappings with Radial Basis Functions with different Sigma values (1)
 a = gendatb([30 30]);
-% make the second class the target class and change the labels:
+% Setting of the second class as target class
 a = oc_set(a,'1');
-% only use target class:
 a = target_class(a);
-% generate test data:
+% Generation of test data
 b = oc_set(gendatb(200),'1');
-
 V = axis; axis(1.5*V);
-% train the individual data descriptions and plot them
-% the error on the target class
+% Training of three SVDDs
 H=[0;0];
 fracrej = 0.2;
 figure(1); clf; hold on;
@@ -55,8 +39,8 @@ H(2)=h;
 legend(H,'Target Data','SVDD with RBF, Sigma=5');
 axis equal;
 axis image;
-%% Preprocessing
-% Partitions Roller Bearing time signals into segments with 5 revolutions 
+%% Preprocessing 
+% Partitions Roller Bearing time signals into segments of 5 revolutions (2,3)
 numberOfRevolutionsPerSegment=5;
 rpm=1796;
 sampleFrequency=48000;
@@ -81,52 +65,68 @@ innerRacewayFaultSegments=SegmentRotationTimeSignal(...
      sampleFrequency,...
      numberOfRevolutionsPerSegment);
  
-%Plot first five revolutions of each signal
+ numberOfDataPoints = size(normalSegments,1);
+ deltaX = numberOfRevolutionsPerSegment/numberOfDataPoints;
+ xScale = (0+deltaX):deltaX:numberOfRevolutionsPerSegment;
+ 
+%Plots the first segment of each dataset
 figure(1); clf;
 subplot(4,1,1), 
-    plot(normalSegments(:,1)),
-    title('Normal');
+    plot(xScale,normalSegments(:,1)),
+    title('First five revolutions of Rolling Element Bearing Data')
+    ylabel('Normal');
 subplot(4,1,2), 
-    plot(ballFaultSegments(:,1)),
-    title('Ball Faults');
+    plot(xScale,ballFaultSegments(:,1)),
+    ylabel('Ball fault');
 subplot(4,1,3), 
-    plot(innerRacewayFaultSegments(:,1)),
-    title('Inner Raceway Faults');
+    plot(xScale,innerRacewayFaultSegments(:,1)),
+    ylabel('IR fault');
 subplot(4,1,4), 
-    plot(outerRacewayFaultSegments(:,1));
-    title('Outer Raceway Faults');
-    %% Extract Features
-    % Extracts Kurtosis (k), Cepstrum Coefficients (c) and Multifractal
-    % Dimensions (m) as 17-tupels in the format (k,c1...c13,m1...,m13) 
+    plot(xScale,outerRacewayFaultSegments(:,1));
+    ylabel('OR fault');
+    %% Feature Extraction
+    % Extracts Kurtosis (k),Mel Frequency Cepstrum Coefficients (c) and Multifractal
+    % Dimensions (m) as 27-tupels in the format (c1...c13,m1...,m13,k) (3)
     normalFeatures = ...
         ExtractRollerBearingFeatures(normalSegments, sampleFrequency);
+    %First normal features vector:
+    normalFeatures(1,:)
     ballFaultFeatures =...
         ExtractRollerBearingFeatures(ballFaultSegments,sampleFrequency);
+    %First ball fault features vector
+    ballFaultFeatures(1,:)
     innerRacewayFaultFeatures =...
         ExtractRollerBearingFeatures(innerRacewayFaultSegments,sampleFrequency);
+    %First inner raceway fault features
+    innerRacewayFaultFeatures(1,:)
     outerRacewayFaultFeatures=...
         ExtractRollerBearingFeatures( outerRacewayFaultSegments,sampleFrequency);
+    %First outer raceway fault features
+    outerRacewayFaultFeatures(1,:)
+    %ToDo: PCA to remove redundancies in dataset
     
-    %% Convert features to PRTools datasets
-    % Creates PR-Tools One-Class Datasets with labels "target" and
-    % "outliers" for training and a dataset for training with target and
-    % outlier samples
-    
+    %% Training and test set generation 
+    % Creates a target dataset with normal features for training and a
+    % dataset with target and outlier samples for testing a Support Vector
+    % Data Description (1)
     normalFeaturesDataSet=dataset(normalFeatures);
     ballFaultFeaturesDataSet=dataset(ballFaultFeatures);
     innerRacewayFaultFeaturesDataset=dataset( innerRacewayFaultFeatures );
     outerRacewayFaultFeaturesDataset=dataset(  outerRacewayFaultFeatures);
-    
+    %Creates a target data training set with normal features
     targetData =...
         gendatoc(normalFeaturesDataSet);
+    %Creates an outlier dataset with ball fault features
     ballFaultOutliers =...
         gendatoc([],  ballFaultFeaturesDataSet);
+    %Creates an outlier dataset with inner raceway fault features
     innerRacewayFaultOutliers=...
         gendatoc([],  innerRacewayFaultFeaturesDataset);
+    %Creates an outlier dataset with outer raceway fault features
     outerRacewayFaultOutliers=...
         gendatoc([],  outerRacewayFaultFeaturesDataset);
    
-   %Create test dataset with 50 random samples of each data set
+   %Creates a test dataset with 50 random samples drawn from each dataset
    numberOfTestSamples=50;
    [targetTestData,targetTrainingData]=...
        gendat(targetData, numberOfTestSamples);
@@ -143,26 +143,28 @@ subplot(4,1,4),
    
 
    %% SVDD Training
-   % Calculates an SVDD mapping with target training data and a
-   % RBF-Kernel with Sigma=5 
-         
+   % Uses the target data to calculate an SVD mapping with a rejection rate of 0.2 and an RBF-Kernel with Sigma=5 (4)     
    w = svdd( targetData,0.2,5);
    
    %% Error evaluation
    % Calculates false negative (rejected targets) and false positive
-   % (accepted outliers) rates and plots a ROC curve.
-   
+   % (accepted outliers) rates and plots a ROC curve (1)
    e = dd_error(testData,w);
-   +e
-   %plot roc curve
+   %False Negatives:
+   e(1)
+   %False Positives:
+   e(2)
+   %Roc
    figure(2); clf;
    plotroc(dd_roc(testData*w),'r');
 
    %% References
-   % (1) "Early Classification Of Bearing Faults Using Hidden Markov Models,
-   %      Gaussian Mixture Models, Mel-Frequency Cepstral Coefficients and
-   %      Fractals" (Marwala et al)
-   % (2) "Support Vector Data Description" (Tax,Duin)
-   %      http://mediamatica.ewi.tudelft.nl/sites/default/files/ML_SVDD_04.pdf
-  
+   %%
+   % # PRTools4, A Matlab Toolbox for Pattern Recognition Version 4.1
+   % (R.P.W. Duin,D.M.J. Tax et al.)
+   % # <http://csegroups.case.edu/bearingdatacenter/pages/welcome-case-western-reserve-university-bearing-data-center-website>
+   % # "Early Classification Of Bearing Faults Using Hidden Markov Models,Gaussian Mixture Models, Mel-Frequency Cepstral Coefficients and Fractals" (Marwala et al)
+   % # "Support Vector Data Description" (Tax,Duin) <http://mediamatica.ewi.tudelft.nl/sites/default/files/ML_SVDD_04.pdf>
+   
+
   
