1623c38f159019d2670227b78bfb2e7079ab16b4
fera0013 <ralphfehrer@web.de>
2013-04-13 21:16:02 +0200

    Roller Bearing outlier detection with Random Forest ctd.
    Plotting of Attribute Accuracy graph added

--- OutlierDetectionRWorkspace/RollerBearingConditionMonitoringWithRandomForests.R
+++ OutlierDetectionRWorkspace/RollerBearingConditionMonitoringWithRandomForests.R
@@ -7,6 +7,12 @@ normalTestVectors<-normalFeatures[normalTestVectorsIndices,]
 normalTrainingVectors<-normalFeatures[-normalTestVectorsIndices,]
 #Construct unsupervised random forest with roller bearings training data set
 rollerBearingNormalRf<-randomForest(normalTrainingVectors[,-28],proximity=TRUE,importance=TRUE)
+#plot variabe importance
+rollerBearingAttributeImportance<-rollerBearingNormalRf$importance[,1]
+sortedRollerBearingAttributeImportance<-sort(rollerBearingAttributeImportance)
+windows.options(width=10, height=50)
+barplot(sortedRollerBearingAttributeImportance, horiz=TRUE,cex.names=0.6,las = 1,main="Normal Features Attribute importance",xlab="Mean Decrease in Accuracy",space=0.5)
+windows.options(reset=TRUE)
 #Calculate class prototype of roller bearing normal data
 rollerBearingNormalClassPrototype <- classCenter(normalTrainingVectors[,-28], normalTrainingVectors[,28], rollerBearingNormalRf$prox)
 #Create Test Set with 30 test vectors of each feature set
@@ -21,5 +27,13 @@ rollerBearingTestSet<-rbind(normalTestVectors,ballFaultTestVectors,outerRacewayF
 EuclideanDistanceToNormalPrototype<-function (vector) {
   return(sqrt(sum((vector- normalFeatureClassPrototype) ^ 2)))
 }
+euclideanDistanceName<-"EuclideanDistance"
 euclideanDistances<-apply(rollerBearingTestSet[,-28],1,EuclideanDistanceToNormalPrototype)
-rollerBearingTestSet["EuclideanDistance"]<-euclideanDistances
+rollerBearingTestSet[euclideanDistanceName]<-euclideanDistances
+#Plot the results
+numericalClassValues<-rep(2,length(euclideanDistances))
+numericalClassValues[rollerBearingTestSet$Class=="Target"]<-1
+plot(euclideanDistances,xlab="Test vectors ",ylab="Euclid. Distance",col=c("red","blue")[numericalClassValues])
+title("Euclidean distances between 
+      Roller Bearing test vectors and target prototype")
+legend("topleft",cex=0.75, inset=.05,c("Normal","Outlier"), pch=c(1,1), col=c("red", "blue"))
